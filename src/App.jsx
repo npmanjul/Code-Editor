b9000dd40132c9f6872f99727f49be27ac9662d7
anjulsingh999 <anjulsingh462@gmail.com>
2024-12-02 19:40:50 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CodeEditor from "./components/CodeEditor";
 function App() {
   return (
     <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+    <div style={{fontSize:34,fontWeight:600,color:"white"}}>XCode Editor</div>
       <CodeEditor />
     </Box>
   );
